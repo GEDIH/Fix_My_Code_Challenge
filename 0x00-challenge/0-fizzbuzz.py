45a594ad45a7cfa7ad06515b6101be2773abe1e0
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2023-05-19 09:31:41 +0300

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """ FizzBuzz
-    Change of logic if (i % 3) == 0 and (i % 5) == 0:
 """
 import sys
-
-
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
